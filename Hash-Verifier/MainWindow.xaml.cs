75b8606736b9a6f1fd30ffeda395982b671cc693
Keil <keilcarpenter@tuta.io>
2019-12-19 11:22:04 +1300

    calculate SHA256

--- Hash-Verifier/MainWindow.xaml.cs
+++ Hash-Verifier/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,17 +30,56 @@ namespace Hash_Verifier
         public void FileDropped(object sender, DragEventArgs e)
         {
             // Check if the data matches the windows drop format
-            if (e.Data.GetDataPresent(DataFormats.FileDrop)){
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
                 // Get data that is in the windows drop format
                 string[] data = (string[])e.Data.GetData(DataFormats.FileDrop);
 
-                GetHash(data[0]);
+                MessageBox.Show(GetHash(data[0]));
             }
         }
 
-        private Byte GetHash(String data)
+        private string GetHash(String data)
         {
+            try
+            {
+                // Get the data from the file
+                FileInfo fileInfo = new FileInfo(data);
 
+                //Create filestream
+                FileStream fileStream = fileInfo.Open(FileMode.Open);
+
+                // Create instance of SHA class
+                SHA256 sha256 = SHA256.Create();
+
+                // Get hash as bytes
+                Byte[] hashValue = sha256.ComputeHash(fileStream);
+
+                fileStream.Close();
+
+                return ConvertBytesToString(hashValue);
+            }
+            catch(IOException e)
+            {
+                return e.ToString();
+            }
+            catch(UnauthorizedAccessException e)
+            {
+                return e.ToString();
+            }
+            
+        }
+
+        private string ConvertBytesToString(Byte[] bytes)
+        {
+            string hashValue = "";
+
+            // Convert bytes to string in hex format
+            foreach(Byte index in bytes){
+                hashValue += index.ToString("x2");
+            }
+
+            return "sha256: " + hashValue;
         }
     }
 }
