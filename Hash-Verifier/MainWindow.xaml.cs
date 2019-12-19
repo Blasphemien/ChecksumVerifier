78f399cda1537d8fad55a97f6c5301f7fa555c63
Keil <keilcarpenter@tuta.io>
2019-12-19 16:01:25 +1300

    return hashValue only and tidy up braces

--- Hash-Verifier/MainWindow.xaml.cs
+++ Hash-Verifier/MainWindow.xaml.cs
@@ -15,70 +15,51 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Hash_Verifier
-{
+namespace Hash_Verifier {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
         }
 
-        public void FileDropped(object sender, DragEventArgs e)
-        {
+        public void FileDropped(object sender, DragEventArgs e) {
             // Check if the data matches the windows drop format
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-            {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
                 // Get data that is in the windows drop format
                 string[] data = (string[])e.Data.GetData(DataFormats.FileDrop);
-
                 MessageBox.Show(GetHash(data[0]));
             }
         }
 
-        private string GetHash(String data)
-        {
-            try
-            {
+        private string GetHash(String data) {
+            try {
                 // Get the data from the file
                 FileInfo fileInfo = new FileInfo(data);
-
                 //Create filestream
                 FileStream fileStream = fileInfo.Open(FileMode.Open);
-
                 // Create instance of SHA class
                 SHA256 sha256 = SHA256.Create();
-
                 // Get hash as bytes
                 Byte[] hashValue = sha256.ComputeHash(fileStream);
-
                 fileStream.Close();
-
                 return ConvertBytesToString(hashValue);
             }
-            catch(IOException e)
-            {
+            catch (IOException e) {
                 return e.ToString();
             }
-            catch(UnauthorizedAccessException e)
-            {
+            catch (UnauthorizedAccessException e) {
                 return e.ToString();
             }
-            
         }
 
-        private string ConvertBytesToString(Byte[] bytes)
-        {
+        private string ConvertBytesToString(Byte[] bytes) {
             string hashValue = "";
-
             // Convert bytes to string in hex format
-            foreach(Byte index in bytes){
+            foreach (Byte index in bytes) {
                 hashValue += index.ToString("x2");
             }
-
             return "sha256: " + hashValue;
         }
     }
