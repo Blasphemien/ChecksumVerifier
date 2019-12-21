13d6a9d97e83b813b075779cdd28c5f25372e62c
Keil <keilcarpenter@tuta.io>
2019-12-21 18:22:18 +1300

    add checksums for SHA1 and MD5

--- Hash-Verifier/MainWindow.xaml.cs
+++ Hash-Verifier/MainWindow.xaml.cs
@@ -18,33 +18,45 @@ namespace Hash_Verifier {
             if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
                 // Get data that is in the windows drop format
                 string[] data = (string[])e.Data.GetData(DataFormats.FileDrop);           
-               textBlock_CheckSum.Text = ("SHA256: " + GetHash(data[0]));
+                textBlock_SHA256.Text = "sha256: " + GetHash(data[0], "sha256");
+                textBlock_SHA1.Text = ("sha1: " + GetHash(data[0], "sha1"));
+                textBlock_MD5.Text = ("md5: " + GetHash(data[0], "md5"));
             }
         }
 
-        private string GetHash(String data) {
-            try {
+        private string GetHash(String data, String hashType) {
+            string checksumType = hashType;
+            Byte[] hashValue = null;
+            try {    
                 // Get the data from the file
                 FileInfo fileInfo = new FileInfo(data);
                 //Create filestream
                 FileStream fileStream = fileInfo.Open(FileMode.Open);
                 // Create instance of SHA class
-                SHA256 sha256 = SHA256.Create();
-                // Get hash as bytes
-                Byte[] hashValue = sha256.ComputeHash(fileStream);
-                fileStream.Close();
+                switch (checksumType) {
+                    case "sha256": 
+                        SHA256 sha256 = SHA256.Create();
+                        hashValue = sha256.ComputeHash(fileStream);
+                        fileStream.Close();
+                        break;
+                    case "sha1":
+                        SHA1 sha11 = SHA1.Create();
+                        hashValue = sha11.ComputeHash(fileStream);
+                        fileStream.Close();
+                        break;
+                    case "md5":
+                        MD5 md5 = MD5.Create();
+                        hashValue = md5.ComputeHash(fileStream);
+                        fileStream.Close();
+                        break;
+                }
                 return ConvertBytesToString(hashValue);
             }
-            catch (IOException e) {
-                return e.ToString();
-            }
-            catch (UnauthorizedAccessException e) {
-                return e.ToString();
-            }
+            catch (IOException e) { return e.ToString(); }
+            catch (UnauthorizedAccessException e) { return e.ToString(); }
         }
-
         private string ConvertBytesToString(Byte[] bytes) {
-            string hashValue = "";
+            string hashValue = null;
             // Convert bytes to string in hex format
             foreach (Byte byteIndex in bytes) {
                 hashValue += byteIndex.ToString("x2");
